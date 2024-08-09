0f7e6c6dc31791127395af5014a66b1ef25fad8d
Khawlaowl <khawlahilli20045@gmail.com>
2024-08-09 15:25:21 +0000

    the server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const PORT = 1337
+
+app.listen(PORT,() => console.log(`server has started on port :${PORT} `))
+
+
+const express = require('express');
+const app = express();
+const PORT = 0000; // Replace with your desired port number
+
+app.listen(PORT, () => {
+    console.log(`Server has started on port: ${PORT}`);
+});
+
