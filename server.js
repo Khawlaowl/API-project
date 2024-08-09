e22cf8330de1521a1436592aea69dfca9ec5ca6f
Khawlaowl <138932395+Khawlaowl@users.noreply.github.com>
2024-08-09 21:55:56 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,33 @@ const DOMAIN = 'http://localhost:1337'
 app.use(express.static('public'));
 
 //routes
+app.post('/create-checkout-session/:product', async (req, res) => {
+    const { product } = req.params
+    let mode, price_ID, line_items, quantity_type
 
+
+    if (product === 'sub') {
+        price_ID = 'price_1MfByvBFgNlxkXGn6Cgrn3iF'
+        mode = 'subscription'
+        line_items = [
+            {
+                price: price_ID,
+            }
+        ]
+        quantity_type = 'subscription'
+    } else if (product === 'pre') {
+        price_ID = 'price_1MfBxpBFgNlxkXGnTdv95Yfd'
+        mode = 'payment'
+        line_items = [
+            {
+                price: price_ID,
+                quantity: 1
+            }
+        ]
+        quantity_type = 10
+    } else {
+        return res.sendStatus(403)
+    }
 app.listen(PORT, () => {
     console.log(`Server has started on port: ${PORT}`);
 });
