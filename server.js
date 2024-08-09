0c9a0675546c929ccce5caf92224133faecde344
Khawlaowl <138932395+Khawlaowl@users.noreply.github.com>
2024-08-09 21:33:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = 1337;
+require('dotenv').config()
+
+//Variables
+const STRIPE_SECRET_KEY = process.env.STRIPE_SK
+const stripe = require('stripe')(STRIPE_SECRET_KEY)
+const DOMAIN = 'http://localhost:1337'
 
 //middleware
 app.use(express.static('public'));
