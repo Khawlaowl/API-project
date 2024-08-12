00dbe9e82d526939f8e4056aa57f822a90509dd4
Khawlaowl <138932395+Khawlaowl@users.noreply.github.com>
2024-08-12 01:21:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express')
 const { generateApiKey } = require('generate-api-key')
 const { db } = require('./firebase')
 const app = express()
-const PORT = 8080
+const PORT = 3000
 require('dotenv').config()
 
 //Variables
 const STRIPE_SECRET_KEY = process.env.STRIPE_SK
 const stripe = require('stripe')(STRIPE_SECRET_KEY)
-const DOMAIN = 'http://34.204.186.114:8080'
+const DOMAIN = 'http://34.204.186.114:3000'
 
 //middleware
 app.use(express.static('public'))
